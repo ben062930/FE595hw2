f4b83b14524450d150e68c14f17b68735c8caed1
hluo13 <71986365+hluo13@users.noreply.github.com>
2020-10-04 19:08:20 -0700

    Update FE595 hw#2.py

--- FE595 hw#2.py
+++ FE595 hw#2.py
@@ -1,22 +1,31 @@
 import requests
 import bs4
+from numpy import arange
+import time
 
+index = list()
+class webscapper():
+    for i in range(0,50):
+        try:
+            web = requests.get('http://3.95.249.159:8000/random_company')
+            web = web.text
+            soup = bs4.BeautifulSoup( web,'html.parser')
+            for x in soup.find_all('li'):
+                x=x.text.split(':')
+                if x[0]=='Name':
+                    index.append(x)
+                if x[0]=='Purpose':
+                    index.append(x)
+        except:
+            index = ["error"]
 
-index=list()
-for i in range(0,50):
-    web = requests.get('http://3.95.249.159:8000/random_company')
-    web = web.text
-    soup = bs4.BeautifulSoup( web,'html.parser')
-    for x in soup.find_all('li'):
-        x=x.text.split(':')
-        if x[0]=='Name':
-            index.append(x)
-        if x[0]=='Purpose':
-            index.append(x)
-    
+    name=index[1::4]
+    purpose=index[3::4]
+    with open("output.txt",'w') as out_file:
+        for t in range(len(index)):
+            res=""
+            res += str(index[t])
+            res += "\n"
+            out_file.write(res)
+            time.sleep(1)
 
-name=index[1::4]
-purpose=index[3::4]
-with open("output.txt",'w') as out_file:
-    out_file.write(str(index))# so that the script will write to the disk just once, but I don't know how to change the row.
-out_file.close()
